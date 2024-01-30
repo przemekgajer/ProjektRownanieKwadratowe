f63790c779b6801c6bf2a4901c107e8331aedb06
przemekgajer <98168330+przemekgajer@users.noreply.github.com>
2024-01-30 18:44:01 +0100

    else
    add else

--- RownanieKwadratowe/RownanieKwadratowe.cs
+++ RownanieKwadratowe/RownanieKwadratowe.cs
@@ -15,7 +15,12 @@ namespace RownanieKwadratowe
             {
                 return new Tuple<double?, double?>(-b / (2 * a), null);
             }
-
+            else
+            {
+                double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                return new Tuple<double?, double?>(x1, x2);
+            }
         }
     }
 }
