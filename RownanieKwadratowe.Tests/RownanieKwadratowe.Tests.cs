1d3a7bd34a9c6528780e561d3703f5ec8080bea9
przemekgajer <98168330+przemekgajer@users.noreply.github.com>
2024-01-30 18:45:43 +0100

    add solver
    solver

--- RownanieKwadratowe.Tests/RownanieKwadratowe.Tests.cs
+++ RownanieKwadratowe.Tests/RownanieKwadratowe.Tests.cs
@@ -3,5 +3,12 @@ using RownanieKwadratowe;
 
 namespace RownanieKwadratowe.Tests
 {
+    public class SolverTests
+    {
+        [Theory]
+        [InlineData(1, -3, 2, 1, 2)]
+        [InlineData(1, 2, 1, -1, null)]
+        [InlineData(1, 0, 1, null, null)]
 
+    }
 }
